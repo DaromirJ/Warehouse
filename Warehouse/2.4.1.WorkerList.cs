12394eac87c6c7775fbc4c532e88583722fa508e
djozw <djozw@DJ-LP50>
2023-02-03 10:55:49 +0100

    Add project files.

---
+++ Warehouse/2.4.1.WorkerList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse
+{
+    internal class WorkerList
+    {
+        static void Main(string[] args)
+        {
+            string imie = "Darisz";
+            string nazwisko = "Jóźwicki";
+            int wiek = 64;
+            string sex = "męszczyzna";
+            string pesel = "59010112288";
+            int nrPrac = 168;
+
+            Console.Write(nrPrac + "." + imie + nazwisko + wiek + sex + pesel);
+        }
+    }
+}
---
+++ Warehouse/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Warehouse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to warehouse app!");
+            Console.WriteLine("Please let me know what you want to do:");
+            Console.WriteLine("1. Add Item");
+            Console.WriteLine("2. Remove Item");
+            Console.WriteLine("3. Chck Item");
+            Console.WriteLine("Press 1, 2 or 3");
+
+            string choice = Console.ReadLine();
+
+            Console.WriteLine($"You have chosen option number: {choice}");
+        }
+    }
+}
