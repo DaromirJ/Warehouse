6c28fb7eb2093c540354864afb7899a0c9c221df
Dariusz Jóźwicki <d.jozwicki@outlook.com>
2023-02-09 09:31:50 +0100

    Warehouse uzupełniony

--- Warehouse/Program.cs
+++ Warehouse/Program.cs
@@ -1,21 +1,86 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
 
 namespace Warehouse
 {
-    class Program
+    public class Program
     {
+        //Stała
+        public const string FILE_NAME = @"C:\WarehouseFiles\ImportFile.xlsx";
         static void Main(string[] args)
         {
+            GenericService<Item> genericItemService = new GenericService<Item>();
+
+            Item itemForGeneric = new Item(1, "Apple");
+            Item item2ForGeneric = new Item(2, "Strawberry");
+            genericItemService.Add(itemForGeneric);
+            genericItemService.Add(item2ForGeneric);
+
+            var items = genericItemService.GetAll();
+
+
+            genericItemService.Remove(item2ForGeneric);
+
+            GenericService<MenuAction> genericActionService = new GenericService<MenuAction>();
+            MenuAction menuAction = new MenuAction(1);
+            genericActionService.Add(menuAction);
+
+            MenuActionService actionService = new MenuActionService();
+            ItemService itemService = new ItemService();
+            actionService = Initialize(actionService);
+
             Console.WriteLine("Welcome to warehouse app!");
-            Console.WriteLine("Please let me know what you want to do:");
-            Console.WriteLine("1. Add Item");
-            Console.WriteLine("2. Remove Item");
-            Console.WriteLine("3. Chck Item");
-            Console.WriteLine("Press 1, 2 or 3");
+            while (true)
+            {
+                Console.WriteLine("Please let me know what you want to do:");
+                var mainMenu = actionService.GetMenuActionsByMenuName("Main");
+                for (int i = 0; i < mainMenu.Count; i++)
+                {
+                    Console.WriteLine($"{mainMenu[i].Id}. {mainMenu[i].Name}");
+                }
 
-            string choice = Console.ReadLine();
+                var operation = Console.ReadKey();
 
-            Console.WriteLine($"You have chosen option number: {choice}");
+                switch (operation.KeyChar)
+                {
+                    case '1':
+                        var keyInfo = itemService.AddNewItemView(actionService);
+                        var id = itemService.AddNewItem(keyInfo.KeyChar);
+                        break;
+                    case '2':
+                        var removeId = itemService.RemoveItemView();
+                        itemService.RemoveItem(removeId);
+                        break;
+                    case '3':
+                        var detailId = itemService.ItemDetailSelectionView();
+                        itemService.ItemDetailView(detailId);
+                        break;
+                    case '4':
+                        var typeId = itemService.ItemTypeSelectionView();
+                        itemService.ItemsByTypeIdView(typeId);
+                        break;
+                    default:
+                        Console.WriteLine("Action you entered does not exist");
+                        break;
+                }
+
+
+            }
+        }
+
+        private static MenuActionService Initialize(MenuActionService actionService)
+        {
+            actionService.AddNewAction(1, "Add item", "Main");
+            actionService.AddNewAction(2, "Remove item", "Main");
+            actionService.AddNewAction(3, "Show details", "Main");
+            actionService.AddNewAction(4, "List of Items", "Main");
+
+            actionService.AddNewAction(1, "Clothing", "AddNewItemMenu");
+            actionService.AddNewAction(2, "Electronics", "AddNewItemMenu");
+            actionService.AddNewAction(3, "Grocery", "AddNewItemMenu");
+            return actionService;
         }
     }
 }
